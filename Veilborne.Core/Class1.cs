1d77cbade98b19a1c25acbbb967181cb20d13261
Rowan Smith <rowanpsmith@yahoo.com.au>
2025-10-09 23:58:23 +1000

    Add `Veilborne.Core` and `Veilborne.Windows` projects to solution
    - Create `Veilborne.Core` library targeting `.NET 9.0` with nullable and implicit usings enabled.
    - Introduce `Veilborne.Windows` application with a basic "Hello, World!" program.
    - Update `VibeGame.sln` to include both projects with correct references.

---
+++ Veilborne.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Veilborne.Core;
+
+public class Class1
+{
+}
---
+++ Veilborne.Windows/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Veilborne.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
